e342882da618c985fad4b9b04deaca8f464eb2bf
Takayoshi Matsuyama <tkys0515@gmail.com>
2025-11-25 05:52:38 +0900

    tkm - add - default config json

--- tkmotion/motion_flow.py
+++ tkmotion/motion_flow.py
@@ -2,7 +2,20 @@ class MotionFlow:
     """A class to handle motion flow operations."""
 
     def __init__(self):
-        pass
+        self.config = []
+
+    def load_config(self):
+        print("Loading configuration...")
+        import json
+        import os
+
+        print(f"{os.getcwd()=}")
+
+        try:
+            with open("tkmotion/default_config.json", "r") as f:
+                self.config = json.load(f)
+        except Exception as e:
+            print(f"Error loading configuration: {e}")
 
     def execute(self):
         print("Executing motion flow...")
